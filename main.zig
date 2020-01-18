b9c41fafef04b22b89d134afcc503d4946840e90
Sebastian <Sebastianhopkins@live.com>
2020-01-18 01:51:00 +0000

    Update main.zig

--- main.zig
+++ main.zig
@@ -1,22 +1,19 @@
 const std = @import("std");
 usingnamespace std.os.windows;
-
 extern "kernel32" fn LoadLibraryA(lpLibFileName: [*:0]const u8) callconv(.Stdcall) ?HMODULE;
-
 pub fn main() !void {
-    const new_module = LoadLibraryA("main.dll") orelse
-        return error.LoadLibraryAFailure;
-
-    const num_proc = kernel32.GetProcAddress(new_module, "GetXKCD") orelse
-        return error.ProcNotFound;
-
-    const myXKCD = @ptrCast(
+    const getXKCD = @ptrCast(
         fn(i64) extern struct {title: [*:0]const u8, url: [*:0]const u8},
-        num_proc
-    )(600);
+        kernel32.GetProcAddress(
+            LoadLibraryA("main.dll") orelse return error.LoadLibraryAFailure,
+            "GetXKCD"
+        ) orelse return error.ProcNotFound
+    );
+
+    const my_xkcd = getXKCD(100);
 
     std.debug.warn("title: {}, url: {}\n", .{
-        std.mem.toSliceConst(u8, myXKCD.title),
-        std.mem.toSliceConst(u8, myXKCD.url)
+        std.mem.toSliceConst(u8, my_xkcd.title),
+        std.mem.toSliceConst(u8, my_xkcd.url)
     });
 }
