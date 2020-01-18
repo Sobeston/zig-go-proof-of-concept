1f1c87d6efa51066704a2a1c46e2100178ac7066
Sebastian <Sebastianhopkins@live.com>
2020-01-18 01:28:11 +0000

    Update main.go

--- main.go
+++ main.go
@@ -11,7 +11,6 @@ func GetXKCD(number int) (*C.char, *C.char, bool) {
 	client := xkcd.NewClient()
 	comic, err := client.Get(number)
 	if err != nil {
-
 		return C.CString(""), C.CString(""), false
 	}
 	return C.CString(comic.Title), C.CString(comic.ImageURL), true
