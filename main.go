ceb1f2bb65b3403572f50692e4aae611c1d709c8
Sebastian <Sebastianhopkins@live.com>
2020-01-22 11:28:15 +0000

    Update main.go

--- main.go
+++ main.go
@@ -7,13 +7,13 @@ import (
 )
 
 //export GetXKCD
-func GetXKCD(number int) (*C.char, *C.char, bool) {
+func GetXKCD(number int) (*C.char, *C.char) {
 	client := xkcd.NewClient()
 	comic, err := client.Get(number)
 	if err != nil {
 		panic(err)
 	}
-	return C.CString(comic.Title), C.CString(comic.ImageURL), true
+	return C.CString(comic.Title), C.CString(comic.ImageURL)
 }
 
 func main() {}
